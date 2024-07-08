27b88119c3557c242b407a673363ac24058459c6
Malharii <mrmalhari96@gmail.com>
2024-07-08 21:35:55 +0530

    crated a restaurants routes and midlevers

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import cors from "cors";
 import mongoose from "mongoose";
 import * as dotenv from "dotenv";
 import MyUserRoute from "./routes/MyUserRoute";
+import { v2 as cloudinary } from "cloudinary";
+import MyRestaurantRoute from "./routes/MyRestaurantRoute";
 
 if (process.env.NODE_ENV !== "production") {
   dotenv.config();
@@ -10,6 +12,12 @@ if (process.env.NODE_ENV !== "production") {
 
 mongoose.connect(process.env.MONGODB_CONECTION_STRING as string).then(() => {
   console.log("connected to mongodb");
+});
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const app = express();
@@ -22,6 +30,7 @@ app.get("/health", async (req: Request, res: Response) => {
 });
 
 app.use("/api/my/user", MyUserRoute);
+app.use("/api/my/restaurant", MyRestaurantRoute);
 
 app.listen(7000, () => {
   console.log("server is running on port 7000");
--- src/middleware/validation.ts
+++ src/middleware/validation.ts
@@ -14,15 +14,36 @@ const handleValidationErrors = async (
 
 export const validateMyUserRequest = [
   body("name").isString().notEmpty().withMessage("Name must be String"),
-  body("addressLine1")
-    .isString()
-    .notEmpty()
-    .withMessage("AddressLine1 must be a String"),
+  body("addressLine1").isString().notEmpty().withMessage("AddressLine1 must be a String"),
   body("city").isString().notEmpty().withMessage("City must be String"),
   body("country").isString().notEmpty().withMessage("Country must be String"),
   // body("mobileNo")
   //   .isString()
   //   .notEmpty()
   //   .withMessage("Mobile-No must be Number"),
+  handleValidationErrors,
+];
+
+export const validateMyRestaurantRequest = [
+  body("restaurantName").notEmpty().withMessage("Restaurant name is required"),
+  body("city").notEmpty().withMessage("City is required"),
+  body("country").notEmpty().withMessage("Country is required"),
+  body("deliveryPrice")
+    .isFloat({ min: 0 })
+    .withMessage("Delivery price must be a positive number"),
+  body("estimatedDeliveryTime")
+    .isInt({ min: 0 })
+    .withMessage("Estimated delivery time must be a postivie integar"),
+  body("cuisines")
+    .isArray()
+    .withMessage("Cuisines must be an array")
+    .not()
+    .isEmpty()
+    .withMessage("Cuisines array cannot be empty"),
+  body("menuItems").isArray().withMessage("Menu items must be an array"),
+  body("menuItems.*.name").notEmpty().withMessage("Menu item name is required"),
+  body("menuItems.*.price")
+    .isFloat({ min: 0 })
+    .withMessage("Menu item price is required and must be a postive number"),
   handleValidationErrors,
 ];
